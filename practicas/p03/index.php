793c84c5d537047f09bbf2356dd3df990f0578b5
JosueVJ <josuevj.1e.49@gmail.com>
2023-09-05 22:23:52 -0600

    Ejercicio 3 hecho

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -29,7 +29,7 @@
         echo '</ul>';
     ?>
 
-<h2>Ejercicio 2</h2>
+    <h2>Ejercicio 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
     <ul>
         <li>$a = “ManejadorSQL”;</li>
@@ -38,15 +38,16 @@
     </ul>
     <p>a. Ahora muestra el contenido de cada variable</p>
     <?php
+        unset($a, $b, $c);
         $a = "ManejadorSQL";
         $b = 'MySql';
         $c = &$a;
 
-        echo $a;
+        echo "\$a: $a";
         echo '<br>';
-        echo $b;
+        echo "\$b: $b";
         echo '<br>';
-        echo $c;
+        echo "\$c: $c";
     ?>
     <p>b. Agrega al código actual las siguientes asignaciones:</p>
     <ul>
@@ -59,11 +60,11 @@
     ?>
     <p>c. Vuelve a mostrar el contenido de cada uno</p>
     <?php
-        echo $a;
+        echo "\$a: $a";
         echo '<br>';
-        echo $b;
+        echo "\$b: $b";
         echo '<br>';
-        echo $c;
+        echo "\$c: $c";
     ?>
     <p>d. Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de
     asignaciones</p>
@@ -71,5 +72,61 @@
         pero el uso & es que apunta a la direccion de memoria de la variable a. 
         Entonces si se modifica a se modifica por consecuente b.
     </p>
+    
+    <h2>Ejercicio 3</h2>
+    <p>3. Muestra el contenido de cada variable inmediatamente después de cada asignación,
+    verificar la evolución del tipo de estas variables (imprime todos los componentes de los
+    arreglo):</p>
+    <ul>
+        <li>$a = “PHP5”;</li>
+        <li>$z[] = &$a;</li>
+        <li>$b = “5a version de PHP”;</li>
+        <li>$c = $b*10;</li>
+        <li>$a .= $b;</li>
+        <li>$b *= $c;</li>
+        <li>$z[0] = “MySQL”;</li>
+    </ul>
+    <p>Evolucion</p>
+    <?php
+        unset($a, $b, $c, $z); /*eliminar de la memoria pq asi se soluciono el problema*/
+        $a = "PHP5";
+        echo "\$a: $a"; /*solo se le agrega una cadena a $a */
+        echo '<br>';
+
+        $z[] = &$a; /*z es una arreglo, en su primer espacio que es 0 almacena la direccion de memoria
+        de la variale $a*/
+        echo "\$z[]: $z[0]";
+        echo '<br>';
+
+        $b = "5a version de PHP"; /*solo se le agrega una cadena a $b */
+        echo "\$b: $b";
+        echo '<br>';
+
+        $c = $b*10; /*en $c se le trata de asignar el resultado de la operacion de tratar de convertir 
+        lo que tiene $b a numero y multiplicarlo por 10. $b inicia con un 5, entonces ese es el numero que agarra
+        y multiplica*/
+        echo "\$c: $c";
+        echo '<br>';
+
+        $a .= $b; /*Concatena lo que hay en $a con $b*/
+        echo "\$a: $a";
+        echo '<br>';
+
+        $b *= $c; /*Vuelve a tratar de hacer una conversion pero se toma el 5 de $b y lo multiolica por el 50 de 
+        $cS*/
+        echo "\$b: $b";
+        echo '<br>';
+
+        $z[0] = "MySQL"; /*Se cambia lo que habia en la posicion 0 de $z, tenia la direccion de $a, pero
+        tambien $a se modifico*/
+        echo "\$z[]: $z[0]";
+        echo '<br>';
+        echo $a;
+        echo $b;
+    ?>
+    
+    <h2>Ejercicio 4</h2>
+    <p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de  la matriz $GLOBALS o del modificador 
+    global de PHP.</p>
 </body>
 </html>
